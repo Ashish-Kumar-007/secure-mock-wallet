fb10749a908982435cd10b01f42bf8a8fb342362
Ash45# <ashishsahoo2015@gmail.com>
2023-07-05 21:46:11 +0530

    updated baseUrl

--- client/src/server.js
+++ client/src/server.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3042",
-  // baseURL: "https://smw-api.vercel.app",
+  // baseURL: "http://localhost:3042",
+  baseURL: "api-smw.vercel.app",
 });
 
 export default server;
