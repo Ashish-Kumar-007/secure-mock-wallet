86858ed3cdc36a951198da4f64a78a9bc38e52d7
Ash45# <ashishsahoo2015@gmail.com>
2023-07-05 21:42:52 +0530

    New file

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img className="navbar__logo" src="logo.png" alt="secure-mock-wallet" />
+    </nav>
+  );
+};
+
+export default Navbar;
