08cd8ce7a127bb58719bc04c1b50cf89d5039255
Ash45# <ashishsahoo2015@gmail.com>
2023-07-10 16:49:13 +0530

    added mongodb atlas

--- server/ConnectDB/connect.js
+++ server/ConnectDB/connect.js
@@ -1,31 +1,21 @@
-import { MongoClient, ServerApiVersion } from ("mongodb");
-const uri =
-  "mongodb+srv://Ashish_NFThing:ujTWqq132bPAxKUn@cluster0.tcmf3.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+import mongoose from "mongoose";
 
 async function connectDB() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } catch (error) {
-    console.error(error);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  mongoose
+    .connect(
+      "mongodb+srv://Ashish_NFThing:ujTWqq132bPAxKUn@cluster0.tcmf3.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      console.log("Connected to MongoDB Atlas");
+      // Start your server or perform any other operations
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB Atlas:", error);
+    });
 }
 
 export default connectDB;
