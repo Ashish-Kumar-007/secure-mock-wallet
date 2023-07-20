2ac7f2deecae42282caaf914fd970e451b669d05
Ash45# <ashishsahoo2015@gmail.com>
2023-07-20 13:21:59 +0530

    updated app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <div className="container-xl flex flex-col justify-center items-center">
       <ToastContainer />
       <Navbar />
-      <div className="card flex flex-col lg:flex-row justify-center items-center mt-5 mx-2">
+      <div className="card flex flex-col lg:flex-row justify-center items-center mt-5 mx-2 my-20">
         <div className="card mt-5 sm:mt-2 p-10">
           <Wallet
             loggedIn={loggedIn}
